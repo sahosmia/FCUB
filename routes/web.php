b5f00cdf1446121a0fd872c11e80e90cd959d188
sahosmia <sahosmia.webdev@gmail.com>
2025-11-02 17:56:47 +0600

    install laravel project

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\CourseController;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Laravel\Fortify\Features;
+
+Route::get('/', function () {
+    return Inertia::render('welcome', [
+        'canRegister' => Features::enabled(Features::registration()),
+    ]);
+})->name('home');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('dashboard');
+    })->name('dashboard');
+
+    Route::get('courses', [CourseController::class, 'index'])->name('courses');
+});
+
+require __DIR__.'/settings.php';
